cc4ccba727a96f4b6989683e8765f7ea8b784fcc
hzy <1257664828@qq.com>
2020-08-26 17:34:58 +0800

    添加OAuth功能

---
+++ server/src/lib/api/Google.ts
@@ -0,0 +1,32 @@
+import { google } from "googleapis";
+
+// https://github.com/googleapis/google-api-nodejs-client
+const auth = new google.auth.OAuth2(
+  process.env.G_CLIENT_ID,
+  process.env.G_CLIENT_SECRET,
+  `${process.env.PUBLIC_URL}/login`
+);
+
+export const Google = {
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  authUrl: auth.generateAuthUrl({
+    access_type: 'oneline',
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile"
+    ]
+  }),
+  // 获取令牌
+  logIn: async (code: string) => {
+    const { tokens } = await auth.getToken(code);
+
+    auth.setCredentials(tokens);
+
+    const { data } = await google.people({ version: "v1", auth }).people.get({
+      resourceName: "people/me",
+      personFields: "emailAddresses,names,photos"
+    });
+
+    return { user: data };
+  }
+}
